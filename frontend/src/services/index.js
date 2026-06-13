e1ae9d99843e17756ced35479d72814e3804f7e7
Matija Malisic <matija.malisic04@gmail.com>
2026-06-13 11:26:29 -0500

    Remove leftover junk files from frontend

--- frontend/src/services/index.js
+++
@@ -1,7 +0,0 @@
-export { request, getAuthToken } from "./http";
-export { parseJwt } from "./utils";
-
-export { authAPI } from "./auth";
-export { accountsAPI } from "./accounts";
-export { transactionsAPI } from "./transactions";
-export { reportingAPI } from "./reporting";
